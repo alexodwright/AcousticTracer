565d68d2b76ef58cb3dfef6dd823947e7959f598
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-23 17:23:45 +0000

    update test_voxel.c

--- core/dev/test_voxel.c
+++ core/dev/test_voxel.c
@@ -5,5 +5,15 @@ int main()
 {
     printf("Voxel!\n");
 
+    AT_Voxel vs = {0};
+    AT_voxel_init(&vs);
+    AT_voxel_bin_append(&vs, 5.0f);
+    AT_voxel_bin_append(&vs, 10.0f);
+    AT_voxel_bin_append(&vs, 15.0f);
+
+    for (size_t i = 0; i < vs.count; i++) {
+        printf("Voxel %zu: %f\n", i, vs.items[i]);
+    }
+
     return 0;
 }
